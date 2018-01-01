5a78515d4caebcc22dfd451b7d76a52fb9b47541
Raghu Betina <raghu@firstdraft.com>
2018-01-01 18:52:03 +0000

    Whitelist columns in RecipeItemState for ActiveAdmin

--- app/admin/recipe_item_state.rb
+++ app/admin/recipe_item_state.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register RecipeItemState do
 
+ permit_params :recipe_id, :item_state_id, :description
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
