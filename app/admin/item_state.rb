b7c6e6c5690ab954bd15b202fcd962aafe692ece
Raghu Betina <raghu@firstdraft.com>
2018-01-01 18:52:00 +0000

    Whitelist columns in ItemState for ActiveAdmin

--- app/admin/item_state.rb
+++ app/admin/item_state.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register ItemState do
 
+ permit_params :item_id, :state_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
