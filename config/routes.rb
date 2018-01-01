0c11a640f11854d179ba10d9c8db22f502e25c5a
Raghu Betina <raghu@firstdraft.com>
2018-01-01 18:51:11 +0000

    Generate items resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # Routes for the Item resource:
+  # CREATE
+  get "/items/new", :controller => "items", :action => "new"
+  post "/create_item", :controller => "items", :action => "create"
+
+  # READ
+  get "/items", :controller => "items", :action => "index"
+  get "/items/:id", :controller => "items", :action => "show"
+
+  # UPDATE
+  get "/items/:id/edit", :controller => "items", :action => "edit"
+  post "/update_item/:id", :controller => "items", :action => "update"
+
+  # DELETE
+  get "/delete_item/:id", :controller => "items", :action => "destroy"
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
